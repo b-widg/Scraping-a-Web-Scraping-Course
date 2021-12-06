20254394f157de3db6f8a390c297baaeade0f9d6
Brian Widgeon <bwidg@brians-mbp.attlocal.net>
2021-12-06 09:06:49 -0800

    Add getSessionUrls to provide URLs to visit

---
+++ get-session-urls.js
@@ -0,0 +1,55 @@
+const os = require('os');
+const axios = require('axios').default;
+const cheerio = require('cheerio');
+
+// getSessionUrls() returns an array of all URLs that need to be visied by visiting the site's enrollment page.
+// The session titles are scraped and used to crearte the URL to each session.
+
+// Converts Session Title into full URL to that session.
+// Example Title: 'Lesson 8. Solutions to Other Web Scraping “Gotchas” You May Encounter'
+// Result shoule be: 'https://gohighbrow.com/Solutions-to-Other-Web-Scraping-Gotchas-You-May-Encounter'
+const formatTitleForUrl = (title) => {
+  formattedTitle = title
+    .replaceAll(` `, `-`)
+    .replaceAll(`’`, ``)
+    .replaceAll(`“`, ``)
+    .replaceAll(`”`, ``)
+    .replaceAll(`"`, ``);
+  formattedTitle = `https://gohighbrow.com/${formattedTitle
+    .split('-')
+    .splice(2)
+    .join('-')}`;
+  return formattedTitle;
+};
+
+const getSessionUrls = () => {
+  axios
+    .get('https://gohighbrow.com/portfolio/build-your-own-web-scraping-tool/')
+    .then((response) => {
+      const html = response.data;
+      const $ = cheerio.load(html);
+
+      $('h5:contains("Course plan")+p', html).each(function () {
+        const titlesString = $(this).text();
+        // Titles are in a string.  split(os.EOL) splits them to an array based on operating
+        // system's end of line character.
+        const sessionTitles = titlesString.split(os.EOL);
+        // Remove last element as it should be '+ Quiz'
+        sessionTitles.pop();
+        // Convert array of session titles to array of foll URLs.
+        const sessionUrls = sessionTitles.map((title) => {
+          return formatTitleForUrl(title);
+        });
+        console.log(sessionUrls);
+        return sessionUrls;
+      });
+    })
+    .catch((error) => {
+      console.log(`ERROR: ${error}`);
+    });
+  // .then(() => {
+  //   console.log(`Well .then(), I guess we're done...`);
+  // });
+};
+
+module.exports = getSessionUrls;
---
+++ index.js
@@ -0,0 +1,3 @@
+const fs = require('fs');
+const cheerio = require('cheerio');
+const getSessionUrls = require('./get-session-urls');
